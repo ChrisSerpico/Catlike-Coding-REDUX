8979b1c7f74622625744c0263242072db82b7ad7
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2018-08-10 17:09:52 -0500

    Continuation of Fractal

--- Constructing a Fractal/Assets/Fractal.cs
+++ Constructing a Fractal/Assets/Fractal.cs
@@ -13,33 +13,64 @@ public class Fractal : MonoBehaviour {
 
 	public float childScale; 
 
+	private Material[] materials; 
+
+	private void InitializeMaterials() {
+		materials = new Material[maxDepth + 1]; 
+		for (int i = 0; i <= maxDepth; i++) {
+			float t = (float) i / maxDepth; 
+			t *= t; 
+			materials[i] = new Material(material); 
+			// Changed from original for truly random colors 
+			materials[i].color = Color.Lerp(Color.white, new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)), t); 
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
+		if (materials == null) {
+			InitializeMaterials(); 
+		}
 		gameObject.AddComponent<MeshFilter>().mesh = mesh; 
-		gameObject.AddComponent<MeshRenderer>().material = material; 
+		gameObject.AddComponent<MeshRenderer>().material = materials[depth]; 
 		if (depth < maxDepth) {
 			StartCoroutine(CreateChildren()); 
 		}
 	}
 
+	private static Vector3[] childDirections = {
+		Vector3.up,
+		Vector3.right,
+		Vector3.left,
+		Vector3.forward,
+		Vector3.back
+	};
+
+	private static Quaternion[] childOrientations = {
+		Quaternion.identity,
+		Quaternion.Euler(0f, 0f, -90f), 
+		Quaternion.Euler(0f, 0f, 90f),
+		Quaternion.Euler(90f, 0f, 0f),
+		Quaternion.Euler(-90f, 0f, 0f)
+	}; 
+
 	private IEnumerator CreateChildren() {
-		yield return new WaitForSeconds(0.5f); 
-		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up, Quaternion.identity);
-		yield return new WaitForSeconds(0.5f); 
-		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));  
-		yield return new WaitForSeconds(0.5f); 
-		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f)); 
+		for (int i = 0; i < childDirections.Length; i++) {
+			yield return new WaitForSeconds(Random.Range(0.1f, 0.5f)); 
+			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, i);
+		}
 	}
 
-	private void Initialize(Fractal parent, Vector3 direction, Quaternion orientation) {
+	private void Initialize(Fractal parent, int childIndex) {
 		mesh = parent.mesh; 
+		materials = parent.materials; 
 		material = parent.material; 
 		maxDepth = parent.maxDepth; 
 		depth = parent.depth + 1; 
 		childScale = parent.childScale; 
 		transform.parent = parent.transform; 
 		transform.localScale = Vector3.one * childScale; 
-		transform.localPosition = direction * (0.5f + 0.5f * childScale); 
-		transform.localRotation = orientation; 
+		transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale); 
+		transform.localRotation = childOrientations[childIndex]; 
 	}
 }
