3a9755cf0e303f55ec07edd71c44e41f0c5ae69e
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2018-09-06 21:51:59 -0500

    Rounded Cube part 2 completed

--- Rounded Cube/Assets/Cube.cs
+++ Rounded Cube/Assets/Cube.cs
@@ -25,6 +25,40 @@ public class Cube : MonoBehaviour {
 							(ySize - 1) * (zSize - 1)) * 2; 
 		vertices = new Vector3[cornerVertices + edgeVertices + faceVertices]; 
 
+		int v = 0; 
+		for (int y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++) {
+				vertices[v++] = new Vector3(x, y, 0); 
+				yield return wait; 
+			}
+			for (int z = 1; z <= zSize; z++) {
+				vertices[v++] = new Vector3(xSize, y, z); 
+				yield return wait; 
+			}
+			for (int x = xSize - 1; x >= 0; x--) {
+				vertices[v++] = new Vector3(x, y, zSize); 
+				yield return wait; 
+			}
+			for (int z = zSize - 1; z > 0; z--) {
+				vertices[v++] = new Vector3(0, y, z); 
+				yield return wait; 
+			}
+		}
+		for (int z = 1; z < zSize; z++) {
+			for (int x = 1; x < xSize; x++) {
+				vertices[v++] = new Vector3(x, ySize, z); 
+				yield return wait; 
+			}
+		}
+		// these are in two loops to keep the vertices grouped together 
+		for (int z = 1; z < zSize; z++) {
+			for (int x = 1; x < xSize; x++) {
+				vertices[v++] = new Vector3(x, 0, z); 
+				yield return wait; 
+			}
+		}
+
+
 		yield return wait;
 	}
 
